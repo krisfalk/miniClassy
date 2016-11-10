f4cb68e4d103902f5d038b5f904d5d41bd73736d
krisfalk <krisfalk2007@hotmail.com>
2016-11-10 15:11:48 -0600

    update my views

--- VirtualEnv/Project/App/Notion/NotionViews.py
+++ VirtualEnv/Project/App/Notion/NotionViews.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models  import Notion
+from App.models  import Notion
 # Create your views here.
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
