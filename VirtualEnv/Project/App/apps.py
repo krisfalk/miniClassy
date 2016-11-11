ef852e6fa8afef0b3707056fccc57e9224271eb9
Nathan <thaonk26@uww.edu>
2016-11-11 09:55:24 -0600

    save

--- VirtualEnv/Project/App/apps.py
+++ VirtualEnv/Project/App/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     name = 'App'
+
+def genericFormLoader(currentList):
+    html = ""
+    for item in currentList:
+        if "id" not in item:
+            html += "<tr><td>%s:</td><td><input name='%s'></td></tr>" %(item.replace("_", " ").capitalize(),item)
+
+    return html
