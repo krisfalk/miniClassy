894bfc5d93f3845349dc92fedd0d15b684bed8ab
krisfalk <krisfalk2007@hotmail.com>
2016-11-11 09:34:14 -0600

    added loading of forms dynamically

--- VirtualEnv/Project/App/Address/AddressViews.py
+++ VirtualEnv/Project/App/Address/AddressViews.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from App.models  import Address
+from App.apps import genericFormLoader
+from App.forms import AddressForm
 # Create your views here.
 def post_create(request):
-	return HttpResponse("<h1>Create</h1>")
+	form = AddressForm(request.POST or None)
+	addressList = []
+	for field in Address._meta.fields:
+		temp = field.get_attname_column()[0]
+		addressList.append(temp)
+
+	string = genericFormLoader(addressList)
+	context = { "form": form, "string": string }
+
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect('/address/create')
+	else:
+		form = AddressForm()
+
+	return render(request, "create.html", context)
 
 def post_detail(request, id):
 	instance = get_object_or_404(Address, id=id)
@@ -22,7 +40,7 @@ def post_list(request):
 		context = {
 			"title": "List"
 		}
-	return reender(request, "index.html", context)
+	return render(request, "index.html", context)
 	#return HttpResponse("<h1>List</h1>")
 
 def post_update(request):
--- VirtualEnv/Project/App/apps.py
+++ VirtualEnv/Project/App/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     name = 'App'
+
+def genericFormLoader(currentList):
+    html = ""
+    for item in currentList:
+        if item != "id":
+            html += "<tr><td>%s:</td><td><input name='%s'></td></tr><br />" %(item,item)
+
+    return html
